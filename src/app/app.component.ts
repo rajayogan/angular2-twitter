6eb9f26617393aaf2cd166dbb656b410b6b7fa06
rajayogan <rajayogan27@gmail.com>
2016-09-05 01:58:02 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  searchquery = '';
+  tweetsdata;
+  
+  constructor(private http: Http){}
+  
+  makecall() {
+    var headers = new Headers();
+    
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+    
+    this.http.post('http://localhost:3000/authorize', {headers: headers}).subscribe((res) => {
+      console.log(res);
+    })
+  }
+  
+  searchcall(){
+    var headers = new Headers();
+    var searchterm = 'query=' + this.searchquery;
+    
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+    
+    this.http.post('http://localhost:3000/search', searchterm, {headers: headers}).subscribe((res) => {
+      this.tweetsdata = res.json().data.statuses;
+    });
+  }
+  
+  usercall(){
+    var headers = new Headers();
+    var searchterm = 'screenname=Yasmin_Payne1';
+    
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+    
+    this.http.post('http://localhost:3000/user', searchterm, {headers: headers}).subscribe((res) => {
+      console.log(res.json().data);
+      
+    });
+  }
+
 }
